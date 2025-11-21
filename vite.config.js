74c62ef0a6ce5083b7bbe1bc8e500bbb9fb946b2
ui jin shin <ujblackjack@gmail.com>
2025-11-21 12:35:05 +0900

    회원가입 완료 mypage 사진 수정 완료 vite.config에 프록시 추가 해서 사진 백과 프론트 연결

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,22 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            },
+            '/uploads': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            }
+        }
+    }
 });
